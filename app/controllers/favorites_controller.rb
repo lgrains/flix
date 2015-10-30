420c45570b2b8bf91b5af0282fce7132d18ebcec
Louise Rains <louise.rains@meyouhealth.com>
2015-10-30 16:06:55 -0400

    add favorite button

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,22 @@
 class FavoritesController < ApplicationController
+  before_action :require_signin
+  before_action :set_movie
+
+  def create
+    @movie.fans << current_user
+
+    redirect_to @movie, notice: "Thanks for fav'ing!"
+  end
+
+  def destroy
+    favorite = current_user.favorites.find(params[:id])
+    favorite.destroy
+    redirect_to @movie, notice: "Sorry you unfaved it!"
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
 end
