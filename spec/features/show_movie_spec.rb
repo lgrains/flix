52a0a770bac29d9b4e47f025b18058b34a7352b8
Louise Rains <louise.rains@meyouhealth.com>
2015-11-16 14:37:28 -0500

    adds action-specific content for movies and users

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -49,4 +49,29 @@ describe "Viewing an individual movie" do
 
     expect(page).to_not have_text("Recent Reviews")
   end
+
+  it "shows the movie's fans and genres in the sidebar" do
+    movie = Movie.create!(movie_attributes)
+
+    user = User.create!(user_attributes)
+    movie.fans << user
+
+    genre = Genre.create!(name: "Action")
+    movie.genres << genre
+
+    visit movie_url(movie)
+
+    within("aside#sidebar") do
+      expect(page).to have_text(user.name)
+      expect(page).to have_text(genre.name)
+    end
+  end
+
+  it "includes the movie's title in the page title" do
+    movie = Movie.create!(movie_attributes)
+
+    visit movie_url(movie)
+
+    expect(page).to have_title("Flix - #{movie.title}")
+  end
 end
